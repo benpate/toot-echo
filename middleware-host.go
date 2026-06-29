e5359ffbcec1b9518cd75c07813380d822d272c1
Ben Pate <ben@pate.org>
2026-06-28 20:14:06 -0600

    Embed trueHostname
    removing from domain tools library

--- middleware-host.go
+++ middleware-host.go
@@ -1,7 +1,8 @@
 package tootecho
 
 import (
-	domaintools "github.com/benpate/domain"
+	"net/http"
+
 	"github.com/labstack/echo/v4"
 )
 
@@ -9,7 +10,21 @@ import (
 func WithHost(next echo.HandlerFunc) echo.HandlerFunc {
 
 	return func(ctx echo.Context) error {
-		ctx.Request().Header.Set("Host", domaintools.Hostname(ctx.Request()))
+		ctx.Request().Header.Set("Host", trueHostname(ctx.Request()))
 		return next(ctx)
 	}
+}
+
+// trueHostname returns the host name from the request, accounting for
+// proxy headers (like X-Forwarded-Host).
+func trueHostname(request *http.Request) string {
+
+	// If this is a proxied request, then use the X-Forwarded-Host header
+	// instead of the Host header
+	if trueHost := request.Header.Get("X-Forwarded-Host"); trueHost != "" {
+		return trueHost
+	}
+
+	// Fallback to the Host header if X-Forwarded-Host is not present
+	return request.Host
 }
